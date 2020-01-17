7e7b7e58d37261bc8dfb5297dab43ea5f0616ee4
roniphire <roni@phirestudio.com>
2020-01-18 02:16:38 +0700

    scrapping

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Test</h1>
-    </div>
-  );
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/user/Users';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
